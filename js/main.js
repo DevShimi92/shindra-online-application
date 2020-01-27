ff68667a9de49c1e81c5b70bec169b49eaf06795
DevShimi <hospitalier.philippe@gmail.com>
2020-01-27 17:52:44 +0100

    Add logo phaser for intro

--- js/main.js
+++ js/main.js
@@ -14,11 +14,26 @@ var game = new Phaser.Game(config);
 
 function preload () // Fonction pour chargé toute ressourece du jeu
 {
-
+    this.load.image("phaserLogo", "resource/picture/phaser_logo.png");
 }
 
 function create () // Fonction pour créer toute objet du jeu
 {
+    // On fait apparaitre le logo phaser puis on le fait disparaitre en effet fondu grâce a la caméra 
+    
+    image = this.add.image(400, 300, 'phaserLogo');
+    image.setDisplaySize(250,250);
+    
+    this.cameras.main.fadeIn(600);
+    this.time.addEvent({
+        delay: 500,
+        callback: ()=>{
+            this.cameras.main.fadeOut(600);
+        },
+        loop: false
+    })
+    
+
 
 
 }
