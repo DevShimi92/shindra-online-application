0c4b9b98045889ed61a827cb70f54c897042e3fc
DevShimi <hospitalier.philippe@gmail.com>
2020-03-24 10:49:28 +0100

    Add login form

--- js/login.js
+++ js/login.js
@@ -20,9 +20,13 @@ var Login = new Phaser.Class({
     {
         console.log('Login');
 
-        var element = this.add.dom(400, 300).createFromCache('nameform');
-        
-        element.setPerspective(800);
+        var element = this.add.dom(this.game.config.width / 2, this.game.config.height / 2).createFromCache('nameform');
+        console.log(this.game.scale.fullScreenTarget);
+        this.game.scale.fullScreenTarget = document.getElementById('wrapper');
+        console.log(this.game.scale.fullScreenTarget);
+        console.log(this.game.scale.getFullscreenTarget());
+
+        element.setPerspective(80);
 
         element.addListener('click');
 
@@ -30,14 +34,20 @@ var Login = new Phaser.Class({
 
             if (event.target.name === 'loginButton')
             {
+                console.log('Loginc');
                 var inputUsername = this.getChildByName('username');
                 var inputPassword = this.getChildByName('password');
 
                 if (inputUsername.value !== '' && inputPassword.value !== '')
                 {
+                    console.log('Logind');
+                    game.scale.removeFullscreenTarget;
+                    console.log(game.scale.fullScreenTarget);
                     session.username = inputUsername.value;
                     session.password = inputPassword.value ;
+                    console.log(session);
                     game.scene.start('load',session);
+                    console.log('LoginE');
                     
                 }
 
--- js/main.js
+++ js/main.js
@@ -11,6 +11,50 @@ var Preloader = new Phaser.Class({
 
     preload: function ()  // Fonction pour chargé toute ressourece du jeu
     {
+            // fix the height to 720
+            if (game.device.os.desktop == false )
+            {
+                const DEFAULT_HEIGHT = 10;
+            } else
+            {
+                const DEFAULT_HEIGHT = window.innerHeight;
+            }
+
+            // adjust the width dynamically based on the device screen ratio
+            const DEFAULT_WIDTH = (window.innerWidth / window.innerHeight) * DEFAULT_HEIGHT
+        
+            const config = {
+              type: Phaser.AUTO,
+              scale: {
+                mode: Phaser.Scale.FIT,
+                autoCenter: Phaser.Scale.CENTER_BOTH,
+                width: DEFAULT_WIDTH,
+                height: DEFAULT_HEIGHT
+              },
+              parent: 'gameForm',
+              scene: [ Preloader ],
+              dom: {
+                createContainer: true
+                },
+            version: "0.0.0"
+              //...
+            }
+    
+            
+            console.log(DEFAULT_WIDTH);
+            console.log(this.game.config.width);
+
+
+        if (( game.device.os.desktop == false ) && ( DEFAULT_WIDTH !== this.game.config.width))
+        {
+            game.destroy(true);   
+            this.game = new Phaser.Game(config);
+            console.log('Resize');
+            
+        }
+            
+
+
         // On charge les prochaines potentiels scenes
         this.load.script('load','js/load.js');
         this.load.script('mainmenu','js/mainmenu.js');
@@ -23,7 +67,29 @@ var Preloader = new Phaser.Class({
     create: function () // Fonction pour créer toute objet du jeu
     {
         console.log('Main');
+       
+        image = this.add.image(this.game.config.width / 2, this.game.config.height / 2, 'phaserLogo');
         
+
+        if ( game.device.os.desktop == false ) // Si on est sur un apparail mobile 
+            {
+                // Attention la gestion de l'orientation se gere dans le config.xml du projet android
+                image.setDisplaySize(this.game.config.width / 2, this.game.config.height / 3);
+            
+
+            }
+        else{
+
+                image.setDisplaySize(this.game.config.width / 2, this.game.config.height / 1.5);
+        
+        
+            }
+
+            console.log(game.device.os.desktop);
+            console.log(this.scale.orientation);
+
+
+
         this.scene.add('load'  , Load , false);
         
         this.scene.add('mainmenu'  , MainMenu , false);
@@ -31,8 +97,6 @@ var Preloader = new Phaser.Class({
 
         // On fait apparaitre le logo phaser puis on le fait disparaitre en effet fondu grâce a la caméra 
 
-        image = this.add.image(360, 240, 'phaserLogo');
-        image.setDisplaySize(250,250);
         this.cameras.main.fadeIn(500);
         this.time.addEvent({
             delay: 1000,
@@ -50,17 +114,20 @@ var Preloader = new Phaser.Class({
 });
 
 
+const DEFAULT_WIDTH  = 800
+const DEFAULT_HEIGHT = 600
+
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT,
     parent: 'gameForm',
     dom: {
         createContainer: true
     },
     scene: [ Preloader ],
-    version: "0.0.0"
+    version: "0.0.0",
 };
 
 var game = new Phaser.Game(config);
